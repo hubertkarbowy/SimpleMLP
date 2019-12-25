e1b319565cb3eccb0c575ce52f254a37162eb12a
Shadasviar <shadasviarus@gmail.com>
2019-12-25 20:08:42 +0100

    Use current directory instead of absolute paths.
    Signed-off-by: Shadasviar <shadasviarus@gmail.com>

--- src/pl/hubertkarbowy/Main.java
+++ src/pl/hubertkarbowy/Main.java
@@ -11,20 +11,21 @@ public class Main {
     final static int imgWidthAndHeight = 28;
     final static int[] layerDefs = new int[]{imgWidthAndHeight*imgWidthAndHeight, 1};
     static MLPNetwork net = new MLPNetwork(layerDefs);
+    final static String currentDir = System.getProperty("user.dir") + "/";
 
     public static void trainAndSave() throws IOException {
-        net.setInputs("/home/h.karbowy/allrepos/SimpleMLP/other/train2");
+        net.setInputs(currentDir + "other/train2");
         System.out.println(net.toString());
         net.train();
         System.out.println("Loss = " + net.binaryCrossEntropyLoss());
-        net.saveModel("/home/h.karbowy/allrepos/SimpleMLP/other/pretrained2.ser");
+        net.saveModel(currentDir + "other/pretrained2.ser");
     }
 
     public static void runPredictions() throws IOException, ClassNotFoundException {
-        String[] images = {"/home/h.karbowy/allrepos/SimpleMLP/other/test1/three_11.png",
-                           "/home/h.karbowy/allrepos/SimpleMLP/other/test1/one_2.png",
-                           "/home/h.karbowy/allrepos/SimpleMLP/other/test1/eight_2.png"};
-        net.restoreModel("/home/h.karbowy/allrepos/SimpleMLP/other/pretrained2.ser");
+        String[] images = {currentDir + "other/test1/three_11.png",
+                           currentDir + "other/test1/one_2.png",
+                           currentDir + "other/test1/eight_2.png"};
+        net.restoreModel(currentDir + "other/pretrained2.ser");
         for (String imgPath : images) {
             BufferedImage img = loadAndRescale(new File(imgPath), imgWidthAndHeight);
             float inputs[] = imgToInputs(img);
